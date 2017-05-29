13df4915121835c4d32f5479b6d51c5be80868dd
Jeremy Litten <jeremy.litten@gmail.com>
2017-05-28 22:37:50 -0400

    In provisioning script add -y to apt-get install command, install curl, and combine npm install line.

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 
 # Install Google Chrome
 # https://askubuntu.com/questions/79280/how-to-install-chrome-browser-properly-via-command-line
-sudo apt-get install libxss1 libappindicator1 libindicator7
+sudo apt-get install -y libxss1 libappindicator1 libindicator7 curl
 wget https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
 sudo dpkg -i google-chrome-beta*.deb
 sudo apt-get install -f -y
@@ -12,5 +12,4 @@ curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install dependencies
-cd /chrome-headless-screenshot-api
-npm install
+cd /chrome-headless-screenshot-api && npm install
