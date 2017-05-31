7e6440a04b05f9c00a3ded2a6dcfb47ba0ae5ec9
Jeremy Litten <jeremy.litten@gmail.com>
2017-05-31 17:59:25 -0400

    Add --no-bin-links flag to npm install command to fix issue with Vagrant on Windows.

--- provision.sh
+++ provision.sh
@@ -12,4 +12,4 @@ curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install dependencies
-cd /chrome-headless-screenshot-api && npm install
+cd /chrome-headless-screenshot-api && npm install --no-bin-links
